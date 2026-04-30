8b2b33dcb09fc57d04dfd4cf1dcc11cf97dacc3c
Anton Maurovic <anton@foogle.com>
2026-05-01 06:13:36 +0930

    Basic rotozoom

--- src/project.v
+++ src/project.v
@@ -62,9 +62,12 @@ module tt_um_algofoogle_dottee(
   wire [5:0] rgb = rgb_gems; //counter[9:4]^6'b11_10_00; // For now, background is just a colour that gets tinted down ('gems' should get optimised out).
   wire [5:0] rgb_gems;
 
+  wire [9:0] rzh;
+  wire [9:0] rzv;
+
   gems #(.DOTBITS(6)) gems1(
-    .h(h),
-    .v(v+counter),
+    .h(rzh),
+    .v(rzv),//+counter),
     .counter(counter),
     .rgb(rgb_gems)
   );
@@ -80,12 +83,32 @@ module tt_um_algofoogle_dottee(
     .logo_hit(logo_hit)
   );
 
+  checkerboard checks(
+    .h(rzh),
+    .v(rzv),
+    // .rgb(rgb_gems)
+  );
+
+  roto rotozoom_displace(
+    .clk(clk),
+    .rst_n(rst_n),
+    .counter(counter),
+    .pix_x(h),
+    .pix_y(v),
+    .rzx(rzh),
+    .rzy(rzv)
+  );
+
   wire in_logo_stripe = (v[8:3] >= 6'b010001) && (v[8:3] <= 6'b101010); // (v>136) && (v<344);
+
+  wire [9:0] crossx = 138;
+  wire [9:0] crossy = 294;
 
   assign {R,G,B} =
     (!video_active)   ? 6'b00_00_00 :
-    (logo_hit)        ? 6'b11_11_11 :
-    (in_logo_stripe)  ? ((rgb>>1)&6'b01_01_01) :
+    // (h==crossx)       ? 6'b00_10_00 :     (v==crossy)       ? 6'b00_10_00 :
+    (logo_hit && 0)        ? 6'b11_11_11 :
+    // (in_logo_stripe)  ? ((rgb>>1)&6'b01_01_01) :
                       rgb;
 
   always @(posedge vsync, negedge rst_n) begin
@@ -97,3 +120,69 @@ module tt_um_algofoogle_dottee(
   end
 
 endmodule
+
+module checkerboard(
+  input [9:0] h,
+  input [9:0] v,
+  output [5:0] rgb
+);
+  assign rgb =
+    (h[9:5]==2 && v[9:5]==2)  ? 6'b11_00_00
+                              : {6{h[6]^v[6]}};
+endmodule
+
+
+module roto(
+  input clk,
+  input rst_n,
+  input [9:0] counter,
+  input [9:0] pix_x,
+  input [9:0] pix_y,
+  output [9:0] rzx,
+  output [9:0] rzy
+);
+
+  // False reg:
+  reg [9:0] c; //counter[9:4];
+
+  always @(*) begin
+    c = 31;
+    c = 32;
+    c = 8; // 45deg.
+    c = counter[9:0]^10'b0000100000;
+  end
+
+  //wire [7:0] XVEC = counter[7:0] ^ {8{counter[8]}}; //(counter[8] ? counter[7:0] : ~counter[7:0]) + 8'b10000000;
+
+  //localparam XVEC = 8'b10110101; // (1/sqrt(2))<<8
+
+  wire signed [5:0] xvec = 6'b001000; //5'b01001; //8'b10010110; // sin(36) = ~0.5878 = 0.10010110
+  wire signed [5:0] yvec = 6'b000000 + ({5{c[5]}} ^ c[4:0]); //5'b01100; //8'b11001111; // cos(36) = ~0.8090 = 0.11001111
+
+  reg signed [14:0] ox, oy, nx, ny;
+
+  always @(posedge clk) begin
+    if (~rst_n || (pix_x==799 && pix_y==524)) begin
+      ox <= 0;
+      oy <= 0;
+      nx <= 0;
+      ny <= 0;
+    end else if (pix_x==799) begin
+      // Go to next line.
+      ox <= ox - yvec;
+      oy <= oy + xvec;
+      nx <= ox - yvec;
+      ny <= oy + xvec;
+    end else begin
+      nx <= nx + xvec;
+      ny <= ny + yvec;
+    end
+  end
+  
+  //wire [9:0] moving_x = pix_x + counter;
+
+  assign rzx = nx[14:4]-(c[5] ? (c<<4)+16+128 : 128-(c<<4));
+  assign rzy = ny[14:4]+(c[5] ? (c<<4)+16 : ~(c<<4));
+
+endmodule
+
