2e26df732cd709862abb54f18c43205e8339698a
Anton Maurovic <anton@foogle.com>
2026-05-20 22:48:04 +0930

    Bring back visuals

--- src/project.v
+++ src/project.v
@@ -343,19 +343,19 @@ module tt_um_algofoogle_dottee(
   wire debug_gem_mode_p = gem_mode[~h[4:3]]; // "Pixels" are 8-wide and there's 4 of them.
 `endif//DEBUG_GEM_MODE_UI
 
-  wire [5:0] rgb_unblanked = (h<320) ? sample_out : {6{dac_out}};
-// `ifdef DEBUG_DAC
-//     (h>=10'd544) ? {2'b00,{2{dac_out}}, 2'b00} :
-// `endif
-// `ifdef DEBUG_GEM_MODE_SHOW
-//     (debug_gem_mode_en) ? {6{debug_gem_mode_p}} :
-// `endif//DEBUG_GEM_MODE_SHOW
-// `ifdef DEBUG_BAR
-//     (debug_bar_en && (debug_limit || debug_progress)) ? {6{fuzz}} :
-// `endif//DEBUG_BAR
-// (
-//     (logo_hit && logo_en) ? logo_color :
-//                           rgb) | whiteout;
+  wire [5:0] rgb_unblanked = //(h<320) ? sample_out : {6{dac_out}};
+`ifdef DEBUG_DAC
+    (h>=10'd544) ? {2'b00,{2{dac_out}}, 2'b00} :
+`endif
+`ifdef DEBUG_GEM_MODE_SHOW
+    (debug_gem_mode_en) ? {6{debug_gem_mode_p}} :
+`endif//DEBUG_GEM_MODE_SHOW
+`ifdef DEBUG_BAR
+    (debug_bar_en && (debug_limit || debug_progress)) ? {6{fuzz}} :
+`endif//DEBUG_BAR
+(
+    (logo_hit && logo_en) ? logo_color :
+                          rgb) | whiteout;
 
   assign {R,G,B} = (!video_active) ? 6'b00_00_00 : rgb_unblanked;
 
@@ -368,7 +368,7 @@ module tt_um_algofoogle_dottee(
     if (~rst_n) begin
       `ifdef DEBUG_TSTART
       frame_counter <= `DEBUG_TSTART;
-      `else      
+      `else
       frame_counter <= 0;
       `endif
 
